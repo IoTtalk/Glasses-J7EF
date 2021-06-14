19d7295eb82ddfcbddb45f79d26c361bab3194f8
EricPwg <dz92286@gmail.com>
2021-06-14 23:16:39 +0800

    Testing separate SA

--- app/src/main/java/org/iii/testjjsdk/SA_Phone.java
+++ app/src/main/java/org/iii/testjjsdk/SA_Phone.java
@@ -2,11 +2,11 @@ package org.iii.testjjsdk;
 
 import android.app.Activity;
 import android.content.Context;
-import android.os.Handler;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.widget.TextView;
-
-import com.jorjin.jjsdk.sensor.SensorDataListener;
-import com.jorjin.jjsdk.sensor.SensorManager;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -16,7 +16,7 @@ import iottalk.DeviceFeature;
 
 import static com.serenegiant.utils.UIThreadHelper.runOnUiThread;
 
-public class SA_J7EF {
+public class SA_Phone {
     private static final int COUNT_OF_SENSORS = 3;
     private static final int COUNT_OF_ORI = 1;
 
@@ -35,15 +35,18 @@ public class SA_J7EF {
 
     private float[] OriData = new float[3];
 
-    private Handler handler = new Handler();
-
-    private SensorManager sensorManager;
+    private SensorManager andSensorManager;
 
     // IoTtalk setting
     public String api_url = "http://panettone.iottalk.tw:11822/csm";
     public String device_model = "J7EF";
-    public String device_name = "J7EF_test";
-    //Set IDFs in this format
+    public String device_name = "J7EF_Phone";
+
+    //Set the push interval
+    public double push_interval = 0.2;
+    public String device_addr = "20210614b";
+    public boolean persistent_binding = true;
+
     public DeviceFeature AccMeterDF = new DeviceFeature("Acceleration-I1", "idf"){
         @Override
         public JSONArray getPushData() throws JSONException {
@@ -73,11 +76,6 @@ public class SA_J7EF {
         }
     };
 
-    //Set the push interval
-    public double push_interval = 0.2;
-    public String device_addr = "20210614a";
-    public boolean persistent_binding = true;
-
     //invoke after DAN finish register
     public void on_register(DAN dan){
         initUIComponent();
@@ -102,79 +100,65 @@ public class SA_J7EF {
     }
 
     private void initUIComponent() {
-        sensorValue[Acc] = activity.findViewById(R.id.text_acc_v);
-        sensorValue[Gyro] = activity.findViewById(R.id.text_gyro_v);
-        sensorValue[Light] = activity.findViewById(R.id.text_light_v);
-        sensorValue[Ori] = activity.findViewById(R.id.text_ori_v);
-        TextView connectTextView = activity.findViewById(R.id.text_connect);
+        sensorValue[Acc] = activity.findViewById(R.id.text_acc_ph_v);
+        sensorValue[Gyro] = activity.findViewById(R.id.text_gyro_ph_v);
+        sensorValue[Light] = activity.findViewById(R.id.text_light_ph_v);
+        sensorValue[Ori] = activity.findViewById(R.id.text_ori_ph_v);
+        TextView connectTextView = activity.findViewById(R.id.text_connect_ph);
         connectTextView.setText("Server : "+api_url+"\nDevice model : "+device_model+"\nDevice name : "+device_name);
     }
 
-    private SensorDataListener sensorDataListener = new SensorDataListener() {
+    SensorEventListener sensorEventListener = new SensorEventListener() {
         @Override
-        public void onSensorDataChanged(int type, float[] values, long l) {
-            updateDataText(type, values);
+        public void onSensorChanged(SensorEvent event) {
+            runOnUiThread(() -> {
+                switch (event.sensor.getType()){
+                    case Sensor.TYPE_ACCELEROMETER:
+                        for (int i=0; i<3; i++){AccData[i] = event.values[i];}
+                        sensorValue[Acc].setText(
+                                context.getString(R.string.sensor_value3, event.values[0], event.values[1], event.values[2]));
+                        break;
+                    case Sensor.TYPE_GYROSCOPE:
+                        for (int i=0; i<3; i++){GyroData[i] = event.values[i];}
+                        sensorValue[Gyro].setText(
+                                context.getString(R.string.sensor_value3, event.values[0], event.values[1], event.values[2]));
+                        break;
+                    case Sensor.TYPE_LIGHT:
+                        LightData[0] = event.values[0];
+                        sensorValue[Light].setText(
+                                context.getString(R.string.sensor_value1f, event.values[0]));
+                        break;
+                    case Sensor.TYPE_ROTATION_VECTOR:
+                        float ori[] = Quaternion2EulerAngles(event.values);
+                        for (int i=0;i<3;i++){
+                            ori[i] = (float) Math.toDegrees(ori[i]);
+                            OriData[i] = ori[i];
+                        }
+                        sensorValue[Ori].setText(
+                                context.getString(R.string.sensor_value3, ori[0], ori[1], ori[2]));
+                }
+            });
+        }
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
         }
     };
 
-    private void updateDataText(int type, float[] data) {
-        runOnUiThread(() -> {
-            switch (type) {
-                case SensorManager.SENSOR_TYPE_ACCELEROMETER_3D:
-                    sensorValue[Acc].setText(
-                            context.getString(R.string.sensor_value3, data[0], data[1], data[2]));
-                    for (int i=0; i<3; i++){ AccData[i] = data[i]; }
-                    break;
-                case SensorManager.SENSOR_TYPE_GYROMETER_3D:
-                    sensorValue[Gyro].setText(
-                            context.getString(R.string.sensor_value3, data[0], data[1], data[2]));
-                    for (int i=0; i<3; i++){ GyroData[i] = data[i]; }
-                    break;
-                case SensorManager.SENSOR_TYPE_AMBIENTLIGHT:
-                    sensorValue[Light].setText(context.getString(R.string.sensor_value1f, data[0]));
-                    for (int i=0; i<1; i++){ LightData[i] = data[i]; }
-                    break;
-                case SensorManager.SENSOR_TYPE_DEVICE_ORIENTATION:
-                    float ori[] = Quaternion2EulerAngles(data);
-                    for (int i=0;i<3;i++){
-                        ori[i] = (float) Math.toDegrees(ori[i]);
-                        OriData[i] = ori[i];
-                    }
-                    sensorValue[Ori].setText(
-                            context.getString(R.string.sensor_value3, ori[0], ori[1], ori[2]));
-                    break;
-            }
-        });
-    }
-
     void registerSensor(){
-        // init glasses sensors
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                sensorManager = new SensorManager(context);
-                sensorManager.addSensorDataListener(sensorDataListener);
-                enableAllGlassesSensor();
-            }
-        });
+        // init phone sensors
+        andSensorManager = (android.hardware.SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        Sensor accSensor = andSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        Sensor gyroSensor = andSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        Sensor rvSensor = andSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        Sensor lightSensor = andSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        andSensorManager.registerListener(sensorEventListener, accSensor, android.hardware.SensorManager.SENSOR_DELAY_FASTEST);
+        andSensorManager.registerListener(sensorEventListener, gyroSensor, android.hardware.SensorManager.SENSOR_DELAY_FASTEST);
+        andSensorManager.registerListener(sensorEventListener, rvSensor, android.hardware.SensorManager.SENSOR_DELAY_FASTEST);
+        andSensorManager.registerListener(sensorEventListener, lightSensor, android.hardware.SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     void unregisterSensor(){
-        disableAllGlassesSensor();
-        sensorManager.removeSensorDataListener(sensorDataListener);
-        sensorManager.release();
-    }
-
-    void enableAllGlassesSensor(){
-        for (int i=0; i<7; i++){
-            sensorManager.open(i);
-        }
-    }
-
-    void disableAllGlassesSensor(){
-        for (int i=0; i<7; i++){
-            sensorManager.close(i);
-        }
+        andSensorManager.unregisterListener(sensorEventListener);
     }
 
     public static float[] Quaternion2EulerAngles(float[] quaternion){
@@ -203,7 +187,7 @@ public class SA_J7EF {
         return angles;
     }
 
-    public SA_J7EF(Context context){
+    public SA_Phone(Context context){
         this.context = context;
         this.activity = (Activity) context;
     }
--- app/src/main/java/org/iii/testjjsdk/SensorIottalkActivity.java
+++ app/src/main/java/org/iii/testjjsdk/SensorIottalkActivity.java
@@ -10,27 +10,33 @@ import iottalk.DAI;
 public class SensorIottalkActivity extends AppCompatActivity {
     private Context context = this;
 
-    private SA sa;
-    private DAI dai;
+    private SA_J7EF saJ7EF;
+    private SA_Phone saPhone;
+    private DAI daiJ7EF;
+    private DAI daiPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor_iottalk);
-        sa = new SA(context);
-        dai = new DAI(sa);
+        saJ7EF = new SA_J7EF(context);
+        saPhone = new SA_Phone(context);
+        daiJ7EF = new DAI(saJ7EF);
+        daiPhone = new DAI(saPhone);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        dai.start();
+        daiJ7EF.start();
+        daiPhone.start();
     }
 
     @Override
     protected void onPause(){
         super.onPause();
-        dai.terminate();
+        daiJ7EF.terminate();
+        daiPhone.terminate();
     }
 
     @Override
